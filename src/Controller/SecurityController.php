932e3b5075ee89ef574936b1bc5c4973886b5aab
Nicolas <nicolas.villetelle@hotmail.fr>
2020-11-13 11:15:55 +0100

    First Commit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login", methods={"POST"})
+     * @param IriConverterInterface $iriConverter
+     * @return JsonResponse|Response
      */
-    public function login()
+    public function login(IriConverterInterface $iriConverter)
     {
         if (!$this->isGranted('IS_AUTHENTICATED_FULLY')){
             return $this->json([
@@ -18,8 +23,15 @@ class SecurityController extends AbstractController
             ], 400);
         }
 
-        return $this->json([
-            'user' => $this->getUser() ? $this->getUser()->getId() : null
-        ]);
+        return new Response(null, 204, [
+            'Location' => $iriConverter->getIriFromItem($this->getUser())
+    ]);
+    }
+
+    /**
+     * @Route ("/logout", name="app_logout")
+     */
+    public function logout(){
+        throw new \Exception('should not be reached');
     }
 }
